4f6f3d5f3486ae56aad827327127a47c5385e1ab
Valentin Huber <valentinchuber@gmail.com>
2021-03-16 21:19:35 +0100

    custom domain handling 2.0

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -13,7 +13,6 @@ const thirdLog = `${emoji.get('rocket')} ${chalk.green('Your app successfully de
     console.log(firstLog)
     await execa.command('npm run build', { stdio: 'inherit' })
     await execa.command('cp dist/index.html dist/404.html')
-    await execa.command('echo "zhaw.valentinhuber.me" > dist/CNAME')
     await execa.command('git --work-tree dist add --all')
     await execa.command('git --work-tree dist commit -m "gh-pages"')
     console.log(secondLog)

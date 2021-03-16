6188b9c2e2bc15a64263bb59805a94187147a06a
Valentin Huber <valentinchuber@gmail.com>
2021-03-16 21:12:58 +0100

    now actually commiting deployment changes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
-  outputDir: path.resolve(__dirname, './docs'),
+  publicPath: process.env.NODE_ENV === 'production' ? '/zhaw.valentinhuber.me/' : '/',
+  // outputDir: path.resolve(__dirname, './docs'),
 };

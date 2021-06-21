4047007d18c1d6c8e247b18e57b36b7270260f5d
Valentin Huber <valentinchuber@gmail.com>
2021-06-21 22:00:01 +0200

    adding reference back to main page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/thin">THIN</router-link> |
-    <a href="https://github.com/riesentoaster/zhaw.valentinhuber.me">GitHub</a>
+    <a href="https://github.com/riesentoaster/zhaw.valentinhuber.me">GitHub</a> |
+    <a href="https://valentinhuber.me/about">About Me</a>
     <div v-if="new RegExp('^/thin(/.+)?$').test(this.$route.path)">
       <router-link to="/thin/dea">DEA</router-link>
     </div>
